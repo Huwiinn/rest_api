08d649a5b05ce3288c9cfc0dc8e376e92179c895
hwuiinn <gnldls8406@gmail.com>
2024-05-27 00:23:07 +0900

    Feat  : POST API 생성, CORS Error를 해결하기 위한 Header 설정

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+// json메서드로 bodyParser를 사용. json 데이터 분석
+// 이 방법은 application/json에 사용하기 좋은 방식임.
+app.use(bodyParser.json());
+
+// CORS 에러는 JS 코드 문제가 아님. 서버단에서만 처리가 가능하다.
+// res.setHeader로 헤더를 설정하여 해결 가능함.
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Header", "Content-Type, Authorization"); // 클라이언트가 요청에 설정할 수 있는 헤더를 지정
+  next();
+});
 
 app.use("/feed", feedRoutes);
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -6,3 +6,16 @@ exports.getPosts = (req, res, next) => {
     ],
   });
 };
+
+exports.writePost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // Create post in DB
+  // 클라이언트에게 리소스 생성이 완료되었다는 것을 알리기 위해서는 200보다는 201 상태코드가 더 적합하다.
+  // 이유 : 상태코드 201은 리소스를 생성했음을 나타낸다.
+  res.status(201).json({
+    message: "게시글을 작성하였습니다.",
+    post: { id: new Date().toISOString(), title, content },
+  });
+};
